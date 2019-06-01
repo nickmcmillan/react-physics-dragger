e83970f7cbab9aaf5fd46fdd2f4464ee212d0c7a
Tucker <trblackw@ncsu.edu>
2019-06-01 17:08:20 -0400

    Add TS dependencies and setup TS (linting) environment

--- rollup.config.js
+++ rollup.config.js
@@ -4,36 +4,44 @@ import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
+import includePaths from 'rollup-plugin-includepaths'
 import svgr from '@svgr/rollup'
-
 import pkg from './package.json'
 
+const includePathOptions = {
+    include: {},
+    paths: ['src/lib', 'src/other'],
+    external: [],
+    extensions: ['.js', '.json', '.html', '.ts', '.tsx']
+}
+
 export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    external(),
-    postcss({
-      modules: true
-    }),
-    url(),
-    svgr(),
-    babel({
-      exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
-    }),
-    resolve(),
-    commonjs()
-  ]
+    input: 'src/index.tsx',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: true
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        external(),
+        postcss({
+            modules: true
+        }),
+        url(),
+        svgr(),
+        babel({
+            exclude: 'node_modules/**',
+            plugins: ['external-helpers']
+        }),
+        resolve(),
+        commonjs(),
+        includePaths(includePathOptions)
+    ]
 }
