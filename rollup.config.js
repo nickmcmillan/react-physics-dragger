a5862c643c9b1987c54020802bc1bf5f4765b36e
Nick McMillan <nick.a.mcmillan@gmail.com>
2019-06-24 14:58:47 +0200

    ♻️ re-scaffolded project with TS

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,41 @@
-import babel from 'rollup-plugin-babel'
+import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
-import includePaths from 'rollup-plugin-includepaths'
 import svgr from '@svgr/rollup'
+
 import pkg from './package.json'
 
-const includePathOptions = {
-    include: {},
-    paths: ['src/lib', 'src/other'],
-    external: [],
-    extensions: ['.js', '.json', '.html', '.ts', '.tsx', '.css']
-}
-
 export default {
-    input: 'src/index.tsx',
-    output: [
-        {
-            file: pkg.main,
-            format: 'cjs',
-            sourcemap: true
-        },
-        {
-            file: pkg.module,
-            format: 'es',
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        external(),
-        postcss({
-            modules: true
-        }),
-        url(),
-        svgr(),
-        babel({
-            exclude: 'node_modules/**',
-            plugins: ['external-helpers']
-        }),
-        resolve(),
-        commonjs(),
-        includePaths(includePathOptions)
-    ]
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: 'named',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    external(),
+    postcss({
+      modules: true
+    }),
+    url(),
+    svgr(),
+    resolve(),
+    typescript({
+      rollupCommonJSResolveHack: true,
+      clean: true
+    }),
+    commonjs()
+  ]
 }
